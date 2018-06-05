fc7e76f9ee1f4642d4452c8bc26308c49d888b6c
Colette 🌩️ 💝 Kerr <ckerr@mdnwinsite.com>
2018-06-05 16:32:24 +0200

    add a software version of the segmented sum to check the gpu result
    This appears to be a further error, at least certain GPUs are returning
    a result that seems nonsensical

--- src/FoldSegBug.hs
+++ src/FoldSegBug.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeOperators         #-}
-{-# LANGUAGE ViewPatterns          #-}
 
 module Main where
 
@@ -12,28 +11,39 @@ import           Data.Array.Accelerate          ((:.), Acc, Exp, Scalar, Vector,
 import qualified Data.Array.Accelerate          as A
 import           Data.Array.Accelerate.LLVM.PTX
 
+dat :: Num a => Int -> [a]
+dat l = [ fromIntegral (x `mod` 100) | x <- [0..(l - 1)]]
 
-segmentedFoldBug :: forall a. (A.ToFloating Int a, A.Floating a, A.Ord a) => Acc (Scalar a)
-segmentedFoldBug  = A.maximum $ A.foldSeg (+) 0 genDat segmentsAcc where
-    segments = seg 0 1 where
-      seg s n
-        | s > 45000000 = []
-        | n > 10       = seg s 1
-        | otherwise    = n : seg (s + n) (n + 1)
-    segSum = sum segments
-    segCt  = length segments
+segments :: [Int]
+segments = seg 0 1 where
+  seg s n
+    | s > 45000000 = []
+    | n > 10       = seg s 1
+    | otherwise    = n : seg (s + n) (n + 1)
+segSum :: Int
+segSum = sum segments
+segCt :: Int
+segCt  = length segments
+
+softwareSegFold :: Num a => [Int] -> [a] -> [a]
+softwareSegFold [] _      = []
+softwareSegFold _ []      = error "ran off end of list"
+softwareSegFold (s:ss) xs = sum (take s xs) : softwareSegFold ss (drop s xs)
+
+segmentedFoldBug :: forall a. (Num a, A.ToFloating Int a, A.Floating a, A.Ord a) => Acc (Scalar a)
+segmentedFoldBug  = A.maximum $ A.fold1Seg (+) genAcc segmentsAcc where
     segmentsAcc :: Acc (Vector Int)
     segmentsAcc = A.use $ A.fromList (Z A.:. segCt) segments
-    genDat :: Acc (Vector a)
-    genDat = A.map A.toFloating $ A.generate (A.constant $ Z A.:. segSum) f where
-      f :: Exp (Z :. Int) -> Exp Int
-      f (A.unlift -> (Z A.:. x)) = A.mod x 100
+    genAcc :: Acc (Vector a)
+    genAcc = A.use $ A.fromList (Z A.:. segSum) (dat segSum)
 
 
 main :: IO ()
 main = do
   let
+    softDoubles = maximum $ softwareSegFold segments (dat segSum)  :: Double
     doublesOkay = segmentedFoldBug :: Acc (Scalar Double)
     floatsFail  = segmentedFoldBug :: Acc (Scalar Float)
+  print softDoubles -- expect 945
   print $ run doublesOkay
   print $ run floatsFail
